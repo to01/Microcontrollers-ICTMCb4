4d85961c4d505676348539445d0ec578544ef1cd
Vosjethom <90611613+Vosjethom@users.noreply.github.com>
2023-11-27 12:49:37 +0100

    start test demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,54 @@
 #include <Adafruit_ILI9341.h>
+#include <Arduino.h>
+#include "Nunchuk.h"
+#include "Wire.h"
 
 #define TFT_DC 9
 #define TFT_CS 10
+#define BAUDRATE 9600
+#define CHUNKSIZE 32
+#define BUFFERLEN 256
+#define NUNCHUK_ADDRESS 0x52
+
+volatile bool previousButtonState = 0;
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 int main(void)
 {
   tft.begin();
-
-  tft.fillRect(0, 0, ILI9341_TFTWIDTH, ILI9341_TFTHEIGHT, ILI9341_BLACK);
-
-  for (int i = 0; i < 150; i++)
-  {
-    tft.fillCircle(25 + i - 4, 25 + i - 4, 5, ILI9341_BLACK);
-    tft.fillCircle(25 + i, 25 + i, 5, ILI9341_BLUE);
-    _delay_ms(150);
-    i += 3;
-#include <Arduino.h>
-#include "Nunchuk.h"
-#include "Wire.h"
-
-#define BAUDRATE	9600
-#define CHUNKSIZE	32
-#define BUFFERLEN	256
-#define NUNCHUK_ADDRESS 0x52
-
-volatile bool previousButtonState = 0;
-
-int main(void)
-{
   init();
   NunChuk Nunchuk;
   Wire.begin();
   Nunchuk.begin(NUNCHUK_ADDRESS);
   Serial.begin(9600);
-  while(1)
+  int posX;
+  int posY;
+
+  tft.fillRect(0, 0, ILI9341_TFTWIDTH, ILI9341_TFTHEIGHT, ILI9341_BLACK);
+
+  while (1)
   {
     Nunchuk.getState(NUNCHUK_ADDRESS);
-    if(previousButtonState != Nunchuk.state.c_button)
+    if (Nunchuk.state.joy_x_axis > 128)
     {
-      //execute something when c is pressed here
+      tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
+      tft.fillCircle(posX + 5, posY, 5, ILI9341_BLUE);
     }
-    previousButtonState = Nunchuk.state.c_button;
+    else if (Nunchuk.state.joy_x_axis < 128)
+    {
+      tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
+      tft.fillCircle(posX - 5, posY, 5, ILI9341_BLUE);
+    }
+    else if (Nunchuk.state.joy_y_axis > 128)
+    {
+      tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
+      tft.fillCircle(posX, posY + 5, 5, ILI9341_BLUE);
+    }
+    else if (Nunchuk.state.joy_y_axis)
+    {
+      tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
+      tft.fillCircle(posX, posY - 5, 5, ILI9341_BLUE);
+    }
   }
 }
