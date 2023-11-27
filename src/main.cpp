4bdf130deec14e4b04adbd53c0eff42c6f8b5ab3
Vosjethom <90611613+Vosjethom@users.noreply.github.com>
2023-11-27 13:48:16 +0100

    basis demo

--- src/main.cpp
+++ src/main.cpp
@@ -16,16 +16,17 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 int main(void)
 {
-  tft.begin();
   init();
+  tft.begin();
   NunChuk Nunchuk;
   Wire.begin();
   Nunchuk.begin(NUNCHUK_ADDRESS);
   Serial.begin(9600);
-  int posX;
-  int posY;
+  double posX = ILI9341_TFTWIDTH / 2;
+  double posY = ILI9341_TFTHEIGHT / 2;
 
   tft.fillRect(0, 0, ILI9341_TFTWIDTH, ILI9341_TFTHEIGHT, ILI9341_BLACK);
+  tft.fillCircle(posX, posY, 5, ILI9341_BLUE);
 
   while (1)
   {
@@ -33,22 +34,23 @@ int main(void)
     if (Nunchuk.state.joy_x_axis > 128)
     {
       tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
-      tft.fillCircle(posX + 5, posY, 5, ILI9341_BLUE);
+      tft.fillCircle(posX += 0.5, posY, 5, ILI9341_BLUE);
     }
     else if (Nunchuk.state.joy_x_axis < 128)
     {
       tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
-      tft.fillCircle(posX - 5, posY, 5, ILI9341_BLUE);
+      tft.fillCircle(posX -= 0.5, posY, 5, ILI9341_BLUE);
     }
     else if (Nunchuk.state.joy_y_axis > 128)
     {
       tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
-      tft.fillCircle(posX, posY + 5, 5, ILI9341_BLUE);
+      tft.fillCircle(posX, posY += 0.5, 5, ILI9341_BLUE);
     }
-    else if (Nunchuk.state.joy_y_axis)
+    else if (Nunchuk.state.joy_y_axis < 128)
     {
       tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
-      tft.fillCircle(posX, posY - 5, 5, ILI9341_BLUE);
+      tft.fillCircle(posX, posY -= 0.5, 5, ILI9341_BLUE);
     }
   }
+  return 0;
 }
