074e4fda8d315e92a7b436bb9d86d639a65696c5
to01 <toudsen@gmail.com>
2023-11-27 14:30:21 +0100

    added analog movement and FPS cap

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include "Nunchuk.h"
 #include "Wire.h"
+#include <avr/delay.h>
 
 #define TFT_DC 9
 #define TFT_CS 10
@@ -22,35 +23,44 @@ int main(void)
   Wire.begin();
   Nunchuk.begin(NUNCHUK_ADDRESS);
   Serial.begin(9600);
-  double posX = ILI9341_TFTWIDTH / 2;
-  double posY = ILI9341_TFTHEIGHT / 2;
+  uint16_t posX = ILI9341_TFTWIDTH / 2;
+  uint16_t posY = ILI9341_TFTHEIGHT / 2;
 
-  tft.fillRect(0, 0, ILI9341_TFTWIDTH, ILI9341_TFTHEIGHT, ILI9341_BLACK);
+  tft.fillRect(0, 0, ILI9341_TFTWIDTH, ILI9341_TFTHEIGHT, ILI9341_WHITE);
   tft.fillCircle(posX, posY, 5, ILI9341_BLUE);
 
   while (1)
   {
+    tft.fillCircle(posX, posY, 5, ILI9341_WHITE);
     Nunchuk.getState(NUNCHUK_ADDRESS);
-    if (Nunchuk.state.joy_y_axis > 128)
-    {
-      tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
-      tft.fillCircle(posX += 0.5, posY, 5, ILI9341_BLUE);
-    }
-    else if (Nunchuk.state.joy_y_axis < 128)
-    {
-      tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
-      tft.fillCircle(posX -= 0.5, posY, 5, ILI9341_BLUE);
-    }
-    else if (Nunchuk.state.joy_x_axis > 128)
-    {
-      tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
-      tft.fillCircle(posX, posY += 0.5, 5, ILI9341_BLUE);
-    }
-    else if (Nunchuk.state.joy_x_axis < 128)
-    {
-      tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
-      tft.fillCircle(posX, posY -= 0.5, 5, ILI9341_BLUE);
-    }
+    posX += (Nunchuk.state.joy_y_axis-127)/32;
+    posY += (Nunchuk.state.joy_x_axis-127)/32;
+    tft.fillCircle(posX, posY, 5, ILI9341_BLUE);
+    _delay_ms(10); // 100FPS
+
+    // tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
+    // tft.fillCircle(posX += 0.5, posY, 5, ILI9341_BLUE);
+    // Nunchuk.getState(NUNCHUK_ADDRESS);
+    // if (Nunchuk.state.joy_y_axis > 128)
+    // {
+    //   tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
+    //   tft.fillCircle(posX += 0.5, posY, 5, ILI9341_BLUE);
+    // }
+    // else if (Nunchuk.state.joy_y_axis < 128)
+    // {
+    //   tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
+    //   tft.fillCircle(posX -= 0.5, posY, 5, ILI9341_BLUE);
+    // }
+    // else if (Nunchuk.state.joy_x_axis > 128)
+    // {
+    //   tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
+    //   tft.fillCircle(posX, posY += 0.5, 5, ILI9341_BLUE);
+    // }
+    // else if (Nunchuk.state.joy_x_axis < 128)
+    // {
+    //   tft.fillCircle(posX, posY, 5, ILI9341_BLACK);
+    //   tft.fillCircle(posX, posY -= 0.5, 5, ILI9341_BLUE);
+    // }
   }
   return 0;
 }
