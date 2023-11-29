05e8c1c71c3932fbe2cbcf9c81a5e7aa9e67be42
to01 <toudsen@gmail.com>
2023-11-29 13:13:00 +0100

    turned display update into function + cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <Adafruit_ILI9341.h>
-#include <Arduino.h>
 #include "Nunchuk.h"
 #include "Wire.h"
-#include <avr/delay.h>
 
 #define TFT_DC 9
 #define TFT_CS 10
@@ -16,6 +14,35 @@ volatile bool previousButtonState = 0;
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
+void updateDisplay(uint16_t *posXp, uint16_t *posYp)
+{
+  tft.fillCircle(*posXp, *posYp, RADIUS_PLAYER, ILI9341_WHITE);
+  Nunchuk.getState(NUNCHUK_ADDRESS);
+  *posXp += (Nunchuk.state.joy_y_axis - 127) / 32;
+  *posYp += (Nunchuk.state.joy_x_axis - 127) / 32;
+
+  if (*posXp < RADIUS_PLAYER)
+  {
+    *posXp = RADIUS_PLAYER;
+  }
+  else if (*posXp > ILI9341_TFTWIDTH - RADIUS_PLAYER - 1)
+  {
+    *posXp = ILI9341_TFTWIDTH - RADIUS_PLAYER - 1;
+  }
+
+  if (*posYp < RADIUS_PLAYER)
+  {
+    *posYp = RADIUS_PLAYER;
+  }
+  else if (*posYp > ILI9341_TFTHEIGHT - RADIUS_PLAYER - 1)
+  {
+    *posYp = ILI9341_TFTHEIGHT - RADIUS_PLAYER - 1;
+  }
+
+  tft.fillCircle(*posXp, *posYp, RADIUS_PLAYER, ILI9341_BLUE);
+  _delay_ms(10); // 100FPS
+}
+
 int main(void)
 {
   init();
@@ -23,40 +50,17 @@ int main(void)
   NunChuk Nunchuk;
   Wire.begin();
   Nunchuk.begin(NUNCHUK_ADDRESS);
-  Serial.begin(9600);
   uint16_t posX = ILI9341_TFTWIDTH / 2;
   uint16_t posY = ILI9341_TFTHEIGHT / 2;
+  uint16_t* posXp = &posX;
+  uint16_t* posYp = &posY;
 
   tft.fillRect(0, 0, ILI9341_TFTWIDTH, ILI9341_TFTHEIGHT, ILI9341_WHITE);
   tft.fillCircle(posX, posY, RADIUS_PLAYER, ILI9341_BLUE);
 
   while (1)
   {
-    tft.fillCircle(posX, posY, RADIUS_PLAYER, ILI9341_WHITE);
-    Nunchuk.getState(NUNCHUK_ADDRESS);
-    posX += (Nunchuk.state.joy_y_axis - 127) / 32;
-    posY += (Nunchuk.state.joy_x_axis - 127) / 32;
-
-    if (posX < RADIUS_PLAYER)
-    {
-      posX = RADIUS_PLAYER;
-    }
-    else if (posX > ILI9341_TFTWIDTH - RADIUS_PLAYER - 1)
-    {
-      posX = ILI9341_TFTWIDTH - RADIUS_PLAYER - 1;
-    }
-
-    if (posY < RADIUS_PLAYER)
-    {
-      posY = RADIUS_PLAYER;
-    }
-    else if (posY > ILI9341_TFTHEIGHT - RADIUS_PLAYER - 1)
-    {
-      posY = ILI9341_TFTHEIGHT - RADIUS_PLAYER - 1;
-    }
-
-    tft.fillCircle(posX, posY, RADIUS_PLAYER, ILI9341_BLUE);
-    _delay_ms(10); // 100FPS
+    updateDisplay(posXp, posYp);
   }
   return 0;
 }
