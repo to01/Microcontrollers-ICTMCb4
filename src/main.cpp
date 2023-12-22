63f2e87695b867b157ace682d3f0d0c65faa8be3
to01 <toudsen@gmail.com>
2023-12-22 12:10:42 +0100

    added IR_communication library

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include <Wire.h>
 #include "Adafruit_ILI9341.h"
 #include "Nunchuk.h"
+#include "IR_Communication.h"
 
-#define TOGGLENUMBER 38 // number of times the IR emitter toggles to send one bit
 #define TFT_DC 9
 #define TFT_CS 10
 #define BAUDRATE 9600
@@ -68,230 +68,8 @@ CodeOpponent codeOpponentArray[4] = {
     CodeOpponent(GameColors(RED, ILI9341_RED), 0)};
 
 #pragma endregion
-#define INITIALONEDURATION 684    // 9ms
-#define INITIALZERODURATION 342   // 4.5ms
-#define DATALENGTH 32             // total amount of bits sent, including logical inverse
-#define ALLOWEDINITIALVARIANCE 32 // allowed variance for initial one and zero
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-
-volatile uint16_t ticksSinceLastUpdate = 0; // used to refresh display at a consistent rate
-
-volatile uint16_t toggleCount = 0; // keeps track of the number of toggles by IR emitter
-
-volatile uint16_t dataToSend; // 16 bit integer sent via IR
-
-/*
-  used to track which bit should be sent,
-  maximum value 32 unless sending new bits, in which case it is set to 64
-*/
-volatile uint8_t bitTurn;
-
-/*
-  returns either 1 or 0 depending on bitTurn, if bitTurn is 16 or above it will send the inverse of the first 16 bits
-*/
-volatile bool getDigitToSend()
-{
-  if (bitTurn >= 16)
-  {
-    return !((dataToSend >> (15 - (bitTurn - 16))) % 2);
-  }
-  return ((dataToSend >> (15 - bitTurn)) % 2);
-}
-
-volatile void sendZero(void)
-{
-  PORTD &= ~(1 << PORTD6);
-}
-
-volatile void sendOne(void)
-{
-  PORTD ^= (1 << PORTD6);
-}
-
-volatile bool sendingIR = false;
-
-volatile bool recievingIR = false;
-
-ISR(INT0_vect)
-{
-  if (!sendingIR)
-  {
-    recievingIR = true;
-  }
-}
-
-void sendIR(void)
-{
-  if (bitTurn < DATALENGTH || bitTurn == 64)
-  {
-    if (bitTurn == 64) // if communication start
-    {
-      if (toggleCount < INITIALONEDURATION) // send 1 for 9ms
-      {
-        sendOne();
-      }
-      else if (toggleCount < INITIALONEDURATION + INITIALZERODURATION) // then send 0 for 4.5ms
-      {
-        sendZero();
-      }
-      else // then start sending data
-      {
-        bitTurn = 0;
-        toggleCount = 0;
-      }
-    }
-    else
-    {
-      if (toggleCount >= TOGGLENUMBER) // reset togglecount and go to next bit if togglecount reaches TOGGLENUMBER
-      {
-        toggleCount = 0;
-        bitTurn++;
-      }
-      if (getDigitToSend())
-      {
-        sendOne();
-      }
-      else
-      {
-        sendZero();
-      }
-    }
-    toggleCount++;
-  }
-  else
-  { // When done sending
-    sendingIR = false;
-  }
-}
-
-/*
-  Returns status of IR reciever, inverted to correspond with sent bits
-*/
-bool getRecieverStatus(void)
-{
-  return !((PIND >> PIND2) % 2);
-}
-
-uint16_t readCount = 0; // number of times certain things have been read, used in different ways in recieveIR()
-
-enum recieveStatus
-{
-  initialOne,
-  initialZero,
-  dataBits,
-  inverseBits
-};
-
-recieveStatus currentRecieveStatus = initialOne; // used to decern the current status of recieveIR()
-uint16_t recievedBits = 0;
-
-void resetRecieveIR(void) // resets all values needed in recieveIR to their starting values
-{
-  recievingIR = false;
-  currentRecieveStatus = initialOne;
-  readCount = 0;
-}
-
-/*
-  Function used by TIMER0 to recieve IR
-*/
-void recieveIR(void)
-{
-  static bool previousValue;
-  static uint8_t bitCount; // used to track the number of bits read so far 
-  static uint16_t currentBits; // temporary variable to keep track of bits
-
-  switch (currentRecieveStatus)
-  {
-  case initialOne: // checks for 9ms 1
-    if (readCount < INITIALONEDURATION - ALLOWEDINITIALVARIANCE)
-    {
-      if (getRecieverStatus())
-      {
-        readCount++;
-      }
-      else
-      {
-        resetRecieveIR();
-      }
-    }
-    else
-    {
-      if (readCount > INITIALONEDURATION)
-      {
-        resetRecieveIR();
-      }
-      else if (!getRecieverStatus())
-      {
-        currentRecieveStatus = initialZero;
-        readCount = 0;
-      }
-    }
-    break;
-  case initialZero: // checks for 4.5ms 0 
-    if (!(readCount < INITIALZERODURATION - ALLOWEDINITIALVARIANCE))
-    {
-      if (readCount > INITIALZERODURATION || getRecieverStatus())
-      {
-        currentRecieveStatus = dataBits;
-        readCount = 0;
-        bitCount = 0;
-        currentBits = 0;
-      }
-    }
-    else
-    {
-      if (getRecieverStatus())
-      {
-        resetRecieveIR();
-      }
-    }
-    readCount++;
-    break;
-  case dataBits: // reads data bits
-    if (readCount == TOGGLENUMBER / 2)
-    {
-      currentBits = (currentBits << 1);
-      currentBits |= previousValue;
-      previousValue = getRecieverStatus();
-    }
-    else if (readCount == TOGGLENUMBER)
-    {
-      readCount = 0;
-      bitCount++;
-    }
-    readCount++;
-    if (bitCount > 16)
-    {
-      currentRecieveStatus = inverseBits;
-      readCount = 0;
-      bitCount = 0;
-    }
-    break;
-  case inverseBits: // currently only used for resetting and setting recievedBits, might be used to check inverse later
-    recievedBits = currentBits;
-    resetRecieveIR();
-    break;
-  }
-}
-
-ISR(TIMER0_COMPA_vect)
-{
-  if (sendingIR)
-  {
-    sendIR();
-  }
-  else if (bitTurn >= DATALENGTH) // makes sure the IR emitter is off while idle
-  {
-    sendZero();
-    if (recievingIR)
-    {
-      recieveIR();
-    }
-  }
-  ticksSinceLastUpdate++;
-}
 
 // function to change the color of the current pin
 void changeColorPin(uint16_t currentPin)
@@ -478,34 +256,20 @@ void setup(void)
   tft.begin();
 }
 
-/*
-  Sets the variables needed to send bits, returns false if not possible (e.g. when sending or recieving IR)
-*/
-bool sendBits(uint16_t bitsToSend)
-{
-  if (!sendingIR && !recievingIR)
-  {
-    dataToSend = bitsToSend;
-    bitTurn = 64;
-    sendingIR = true;
-    return true;
-  }
-  return false;
-}
-
 int main(void)
 {
   setup();
 
   tft.fillScreen(ILI9341_WHITE);
   drawCodeOpponent();
+  sendBits(256);
   while (1)
   {
-    if (ticksSinceLastUpdate > FPS) // 100FPS
+    if (getTicksSinceLastUpdate() > FPS) // 100FPS
     {
       selectPin();
       getColorCodeBinary();
-      ticksSinceLastUpdate = 0;
+      setTicksSinceLastUpdate(0);
     }
 
     if (selectPinCount > SELECTEDPINCOUNT)
